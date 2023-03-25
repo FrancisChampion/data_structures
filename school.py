2967aa2cdaa7ec967538ab88157783d35084a7a4
Francischampion <francs.amegbor@warwick.ac.uk>
2023-03-25 20:12:13 +0000

    school

--- school.py
+++ school.py
@@ -1,1 +1,14 @@
-print('school is good')
+
+class Restaurant:
+    def _init_(self, name, cuisine_type):
+        self.name = name
+        self.cuisine_type = cuisine_type
+    def describe_restaurant(self):
+        print(f"Restaurant name: {self.name} ")
+        print(f"Cuisine type:  {self.cuisine_type}")
+    def open_restaurant(self):
+        print("Restaurant opens from 6pm-12pm")
+
+restaurant_1 = Restaurant('Delhi76','Indian food')
+restaurant_1.describe_restaurant()
+restaurant_1.open_restaurant() 
